8d495141294dbc2ab410d47a4f2e7b9a5d2367c9
ilham2025 <ilhamabdulaziz2020@gmail.com>
2021-06-23 11:01:49 +0700

    tugaskalkulatorr

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div id="app">
+   <Heading/>
+   <calculator/>
+ </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Heading from "./components/Heading.vue";
+import Calculator from "./components/Calculator.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Heading,
+    Calculator
+  },
+};
 </script>
 
 <style>
@@ -20,7 +24,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #1471ce;
+  margin-top: 30px;
 }
 </style>
---
+++ src/components/Heading.vue
@@ -0,0 +1,19 @@
+<template>
+ <h1>{{message}}</h1>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            message : "Calculator Ahyar"
+        }
+    }
+}
+</script>
+
+<style>
+h1{
+    color: #fff;
+}
+</style>
